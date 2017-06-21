3da859ed1944aca0868815c9c317a80246ddb387
Daniel Pyrathon <daniel@coffeemeetsbagel.com>
2017-06-20 20:57:16 -0700

    # This is a combination of 3 commits. # This is the 1st commit message:
    adds photo fallback
    
    # The commit message #2 will be skipped:
    
    # added a crop size
    
    # The commit message #3 will be skipped:
    
    # remove photo constraint

---
+++ pybay/templatetags/get_speaker_url_with_fallback.py
@@ -0,0 +1,18 @@
+from django.template import Library
+from django.conf import settings
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from easy_thumbnails.files import get_thumbnailer
+
+
+register = Library()
+
+
+@register.simple_tag
+def speaker_url_with_fallback(speaker, crop_size):
+    if bool(speaker.photo):
+        selected_photo = speaker.photo
+        return get_thumbnailer(selected_photo).get_thumbnail(
+            {'size': (crop_size, crop_size), 'crop': True}
+        ).url
+    else:
+        return static(settings.DEFAULT_FALLBACK_IMAGE)
