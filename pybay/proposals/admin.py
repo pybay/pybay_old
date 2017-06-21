a00ba09490a2dd49c6e57d3b998c5b6912b28e89
Yann Kaiser <kaiser.yann@gmail.com>
2017-06-21 04:53:15 +0200

    Revert "Implemented speaker detail page and enchanced model"

--- pybay/proposals/admin.py
+++ pybay/proposals/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 
 from .models import TalkProposal, TutorialProposal
 
-
 class TalkProposalAdmin(admin.ModelAdmin):
-    list_display = ('title', 'speaker', 'status')
-
+    list_display = ('title', 'speaker')
+    pass
 
 admin.site.register(TalkProposal, TalkProposalAdmin)
 admin.site.register(TutorialProposal)
