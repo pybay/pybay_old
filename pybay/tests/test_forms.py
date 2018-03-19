1f4231e7b843f8b2f7827678ec4315ff9e1e7cbe
Bethany Garcia <bethanymgarcia@gmail.com>
2018-03-18 18:46:40 -0700

    Changed cfp talk category descriptions.

--- pybay/tests/test_forms.py
+++ pybay/tests/test_forms.py
@@ -15,7 +15,7 @@ class CfpFormTestCase(TestCase):
             'email': "pirosb3@gmail.com",
             'website': "woo.com",
             'phone': "+14155289519",
-            'category': "python fundamentals and language internals",
+            'category': "Python Fundamentals and Language Internals",
             'audience_level': 1,
             'talk_length': 25,
             'speaker_bio': 'wooo',
