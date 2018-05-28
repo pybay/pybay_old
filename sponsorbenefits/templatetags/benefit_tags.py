4de0b750112b7394973d9e5ad0136b286f3e87c4
Simeon Franklin <sfranklin@twitter.com>
2018-05-28 15:17:15 -0700

    Add tags for loading new benefit types

--- sponsorbenefits/templatetags/benefit_tags.py
+++ sponsorbenefits/templatetags/benefit_tags.py
@@ -3,13 +3,23 @@ from django import template
 from django.utils.safestring import mark_safe
 from django.template.defaultfilters import stringfilter
 
-from sponsorbenefits.models import BenefitRow
+from sponsorbenefits.models import AddOnBenefitRow, AlaCarteBenefitRow, BenefitRow
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def load_benefits(context):
     context['benefits'] = BenefitRow.objects.all().order_by('order')
+    return ''
+
+@register.simple_tag(takes_context=True)
+def load_alacarte_benefits(context):
+    context['benefits'] = AlaCarteBenefitRow.objects.all().order_by('order')
+    return ''
+
+@register.simple_tag(takes_context=True)
+def load_addon_benefits(context):
+    context['benefits'] = AddOnBenefitRow.objects.all().order_by('order')
     return ''
 
 @register.filter
@@ -24,3 +34,9 @@ def checkmark(text):
 def comma2br(text):
     output = "<br>".join(text.split(","))
     return mark_safe(output)
+
+@register.filter
+@stringfilter
+def count_content_lines(text):
+    output = [line for line in text.strip().split("\n")[:-1] if line.strip()]
+    return len(output)
