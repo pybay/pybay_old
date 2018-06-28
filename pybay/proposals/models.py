c121656dbd2b69eff2719151eb53faa5495373a5
Stefano Rivera <stefano@rivera.za.net>
2018-06-27 23:51:41 -0700

    Shorten theme descriptions

--- pybay/proposals/models.py
+++ pybay/proposals/models.py
@@ -4,11 +4,11 @@ from symposion.proposals.models import ProposalBase
 
 
 THEMES = {
-    'python': 'Python Fundamentals & Popular Libraries',
-    'ai': 'Machine Learning, AI, & All things Data',
-    'automation': 'DevOps, Automation, & Testing',
-    'speed': 'Dealing with Speed, Scale, & Performance',
-    'community': 'Hardware Fun & People side of Engineering',
+    'python': 'Python & Libraries',
+    'ai': 'AI & Data',
+    'automation': 'DevOps & Automation',
+    'speed': 'Scale & Performance',
+    'community': 'Hardware & People',
 }
 
 
